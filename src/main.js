f536d8fb8cc642fd30a33a431c1d9fb6c5a9d8f1
PedroCR5 <123810586+PedroCR5@users.noreply.github.com>
2025-11-25 14:21:32 +0100

    responsive2

--- src/main.js
+++ src/main.js
@@ -36,4 +36,3 @@ gameMemory();
 selectGameToPlay();// Click para seleccionar el juego
 returnToChangeTheGame();// Click para volver a la pantalla inicial y resetear la partida jugada
 //? Responsive
-//? Quitar dados del h1
